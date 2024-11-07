98babdb3db1e893861c277059bf1d8de997d15e3
anilsinthu114 <anilsinthu114@gmail.com>
2024-11-07 12:42:56 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -717,6 +717,7 @@ export default function Home() {
                                 <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
                             </svg>
                         </a>
+                        <p className="text-gray-300">Designed and Developed by  <a href="https://anilsinthu.vercel.app" target="_blank" rel="noopener" className="text-gray-300 hover:text-white transition-all duration-200 transform hover:scale-110">Anil Sinthu</a>© 2024</p>
                     </div>
                 </motion.div>
             </footer>
